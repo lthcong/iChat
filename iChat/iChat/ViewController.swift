d1d20513339fe5d37e3ba58c115e4cfd293af6d1
congla <lthcongit@gmail.com>
2020-04-22 09:21:47 +0700

    moving from GitLab

---
+++ iChat/iChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by Cong La on 4/20/20.
+//  Copyright © 2020 Cong La. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
