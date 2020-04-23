7f8e0f75d6cc3c195a7cc15692df397c04be5d04
congla <lthcongit@gmail.com>
2020-04-23 15:12:54 +0700

    building UI

---
+++ iChat/iChat/TableViewCell/PeopleCell/PeopleCell.swift
@@ -0,0 +1,24 @@
+//
+//  PeopleCell.swift
+//  iChat
+//
+//  Created by Cong La on 4/23/20.
+//  Copyright © 2020 Cong La. All rights reserved.
+//
+
+import UIKit
+
+class PeopleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
