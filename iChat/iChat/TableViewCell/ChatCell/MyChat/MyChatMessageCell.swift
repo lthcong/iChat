5f1ed80c2d58cd15f08cff8db81b95e1d151cd10
congla <lthcongit@gmail.com>
2020-04-23 15:00:22 +0700

    building UI

---
+++ iChat/iChat/TableViewCell/ChatCell/MyChat/MyChatMessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyChatMessageCell.swift
+//  iChat
+//
+//  Created by Cong La on 4/23/20.
+//  Copyright © 2020 Cong La. All rights reserved.
+//
+
+import UIKit
+
+class MyChatMessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
